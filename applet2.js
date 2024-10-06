401da4c8577f3b78ec128ab490e0c76721503506
giovbars <giovannybarsobia04@gmail.com>
2024-10-06 21:47:04 +0800

    applet map

---
+++ applet2.js
@@ -0,0 +1,34 @@
+class LeafletMap {
+
+    constructor(containerId, center, zoom) {
+      this.map = L.map(containerId).setView(center, zoom);
+      this.initTileLayer();
+    }
+  
+    initTileLayer() {
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+    }
+  
+    addMarker(lat, lng, message) {
+      const marker = L.marker([lat, lng]).addTo(this.map);
+      marker.bindPopup(message);
+    }
+  
+    loadMarkersFromJson(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(marker => {
+            this.addMarker(marker.latitude, marker.longitude, marker.message);
+          });
+        })
+        .catch(error => console.error('Error loading markers:', error));
+    }
+  }
+  
+  const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+  
+  myMap.loadMarkersFromJson('applet2.json');
