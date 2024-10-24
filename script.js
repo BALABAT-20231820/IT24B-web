3d5212aab921e4b84703cac2b6da2f63a0ac83c6
BALABAT-20231820 <harkenbalabat@gmail.com>
2024-10-24 19:11:39 +0800

    added init method

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ class CardSearch {
         this.cards = this.cardContainer.querySelectorAll('.col-md-4');
         this.init();
 }
+init() {
+    this.searchInput.addEventListener('input', () => {
+        this.filterCards(this.searchInput.value);
+    });
+}
 }
