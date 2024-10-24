d5631dcc8f3f7b15328f6e9b469ef1c2caa2765d
BALABAT-20231820 <harkenbalabat@gmail.com>
2024-10-24 19:10:44 +0800

    added constuctor and parameter

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 class CardSearch {
-    
+    constructor(searchInputId, cardContainerId) {
+        this.searchInput = document.getElementById(searchInputId);
+        this.cardContainer = document.getElementById(cardContainerId);
+        this.cards = this.cardContainer.querySelectorAll('.col-md-4');
+        this.init();
+}
 }
