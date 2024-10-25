9dfc96f3df890432d02db31a4affc5e6f9c10875
BALABAT-20231820 <harkenbalabat@gmail.com>
2024-10-26 00:28:10 +0800

    added async fetchWeatherByLocation

--- applet5.js
+++ applet5.js
@@ -51,6 +51,26 @@ class WeatherService extends WeatherApp {
         } else {
             alert('Please enter a city name.');
         }
-        
+    }
+    async fetchWeatherByLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                async (position) => {
+                    const { latitude, longitude } = position.coords;
+                    const data = await this.getWeatherDataByCoordinates(latitude, longitude);
+                    if (data) {
+                        this.displayWeather(data);
+                        this.cityInput.value = '';
+                    } else {
+                        alert('Unable to retrieve weather data for your location.');
+                    }
+                },
+                () => {
+                    alert('Unable to retrieve your location. Please allow location access.');
+                }
+            );
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
     }
 }
