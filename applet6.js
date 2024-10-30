c7b93fdbcf6b21bec536f1ea60e63373b52c73c6
BALABAT-20231820 <harkenbalabat@gmail.com>
2024-10-30 20:34:48 +0800

    added if

--- applet6.js
+++ applet6.js
@@ -8,7 +8,9 @@ class Todolist {
         this.addButton.addEventListener('click', () => this.addOrUpdateTask());
         this.todoList.addEventListener('click', (e) => {
             const action = e.target.classList.contains('removeButton') ? 'remove' :
-            e.target.classList.contains('editButton') ? 'edit' : 
+                           e.target.classList.contains('editButton') ? 'edit' : 
+                           e.target.classList.contains('doneButton') ? 'done' : null;
+                if (action) this[action + 'Task'](e);
         });
     }
 }
